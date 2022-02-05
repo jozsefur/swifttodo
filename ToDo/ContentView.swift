429740b0602296ea252401b305ad4834ee88e51d
Jozsef Basiszta <jozsef.basiszta@gmail.com>
2022-02-05 20:43:16 +0100

    [app] Basic layout of main view

--- ToDo/ContentView.swift
+++ ToDo/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var dataStore: DataStore
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List() {
+                ForEach(dataStore.toDos) { toDo in
+                    Button {
+                        
+                    } label: {
+                        Text(toDo.name)
+                            .strikethrough(toDo.completed)
+                            .foregroundColor(toDo.completed ? .green : Color(.label))
+                    }
+                }
+            }
+            .listStyle(InsetGroupedListStyle())
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("My ToDos")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+            }
+        }
     }
 }
 
