e0e184dda6cc0c9aa54aa28ec2eae24d2db230b0
Jozsef Basiszta <jozsef.basiszta@gmail.com>
2022-02-05 16:12:36 +0100

    [app] New project - ToDo

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Jozsef Basiszta on 2022. 02. 05..
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
